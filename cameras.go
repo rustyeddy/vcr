4a8ddae60ba661f65b0cd11da74cf412858a03f8
Rusty Eddy <rusty@radianlaser.com>
2021-07-08 08:17:22 -0700

    Cameras are now added from JSON formatted mqtt messages

--- cameras.go
+++ cameras.go
@@ -13,18 +13,6 @@ func init() {
 	cameras = make(map[string]*Camera)
 }
 
-type Camera struct {
-	Name string `json:"name"`
-	Addr string `json:"addr"`
-	Port int	`json:"port"`
-}
-
-func NewCamera(name string) *Camera {
-	cam := &Camera{Name: name, Addr: name, Port: 8080}
-	cameras[name]  = cam
-	return cam;
-}
-
 func GetCameras(w http.ResponseWriter, r *http.Request) {
 	clist := GetCameraList()
 	json.NewEncoder(w).Encode(clist)
